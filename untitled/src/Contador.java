ca61bfbc2bb996fb0634d2be3b17f23e19a592b4
Esther <60114163+Scorpionx7@users.noreply.github.com>
2024-04-25 10:20:50 -0300

    Segundo desafio concluido.

---
+++ untitled/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametroInvalidoException e){
+            System.out.println(e.getMessage());
+        }
+        terminal.close();
+    }
+    static void contar(int parametroUm, int parametroDois) throws  ParametroInvalidoException{
+        if (parametroUm >= parametroDois){
+            throw new ParametroInvalidoException("O segundo parametro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
